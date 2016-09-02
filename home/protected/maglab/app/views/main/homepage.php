df9c34c2bd215ff94aa686966555718535705e7a
Kia Kroas <kiafaldorius@gmail.com>
2016-09-01 22:24:08 -0700

    trying socialicons fonts from perfect icons

--- home/protected/maglab/app/views/main/homepage.php
+++ home/protected/maglab/app/views/main/homepage.php
@@ -10,7 +10,7 @@
     <title><?php echo htmlspecialchars($title,ENT_QUOTES,'UTF-8'); ?></title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/bootstrap-theme.min.css" rel="stylesheet">
-    <link href="/css/main.css">
+    <link href="/css/maglab.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Eczar:600|Raleway:400,400i|Bree+Serif" rel="stylesheet">
     <!--[if IE]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -32,17 +32,17 @@
       //'
     //]]>
     </script>
-    <div id="header" class="container-fluid" style="background: #eee url(&#039;/images/bg/stainless_steel.jpg&#039;) repeat fixed center; padding: 20px;">
+    <div id="header" class="container-fluid">
       <div class="container">
         <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2">
             <a href="/">
-              <image <?php echo MtHaml\Runtime::renderAttributes(array(array('src', '/images/logo.png'), array('title', ('MAGLab Logo')), array('style', ('width: 100%;'))), 'html5', 'UTF-8'); ?>></image>
+              <image <?php echo MtHaml\Runtime::renderAttributes(array(array('id', 'logo'), array('src', '/images/logo.png'), array('title', ('MAGLab Logo'))), 'html5', 'UTF-8'); ?>></image>
             </a>
           </div>
         </div>
       </div>
-      <nav <?php echo MtHaml\Runtime::renderAttributes(array(array('class', ('navbar' . ' ' . 'navbar-default')), array('style', ('margin-top: 25px;'))), 'html5', 'UTF-8'); ?>>
+      <nav class="navbar navbar-default">
         <div class="container">
           <div class="navbar-header">
             <button <?php echo MtHaml\Runtime::renderAttributes(array(array('class', ('navbar-toggle' . ' ' . 'collapsed')), array('type', ('button')), array('data-toggle', ('collapse')), array('data-target', ('#main-nav'))), 'html5', 'UTF-8'); ?>>
@@ -51,18 +51,12 @@
             </button>
           </div>
           <div id="main-nav" class="collapse navbar-collapse">
-            <ul <?php echo MtHaml\Runtime::renderAttributes(array(array('class', ('nav' . ' ' . 'navbar-nav')), array('style', ('margin: 0 auto; display: table; table-layout: fixed; float: none;'))), 'html5', 'UTF-8'); ?>>
-              <li>
-                <a <?php echo MtHaml\Runtime::renderAttributes(array(array('href', ('/'))), 'html5', 'UTF-8'); ?>>Home</a>
-              </li>
+            <ul class="nav navbar-nav">
               <li>
                 <a <?php echo MtHaml\Runtime::renderAttributes(array(array('href', ('/membership'))), 'html5', 'UTF-8'); ?>>Membership</a>
               </li>
               <li>
                 <a <?php echo MtHaml\Runtime::renderAttributes(array(array('href', ('/wiki'))), 'html5', 'UTF-8'); ?>>Wiki</a>
-              </li>
-              <li>
-                <a <?php echo MtHaml\Runtime::renderAttributes(array(array('href', ('https://www.facebook.com/MAGLaboratory'))), 'html5', 'UTF-8'); ?>>Facebook</a>
               </li>
               <li>
                 <a <?php echo MtHaml\Runtime::renderAttributes(array(array('href', ('https://groups.google.com/forum/?fromgroups#!forum/maglaboratory'))), 'html5', 'UTF-8'); ?>>Forum / Mailing list</a>
@@ -80,6 +74,31 @@
           </div>
         </div>
       </nav>
+      <div class="container">
+        <ul class="social-icons">
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-email1'), array('href', ('mailto:contact@maglaboratory.org'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-facebook'), array('href', ('https://www.facebook.com/MAGLaboratory'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-instagram'), array('href', ('https://www.instagram.com/MAGLaboratory/'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-youtube'), array('href', ('https://www.youtube.com/channel/UCxkmJiwGmDQnIvLe2gDKbpg'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-twitter'), array('href', ('https://www.twitter.com/MAGLaboratory'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', 'soc-github'), array('href', ('https://github.com/MAGLaboratory'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+          <li>
+            <a <?php echo MtHaml\Runtime::renderAttributes(array(array('class', ('soc-yelp' . ' ' . 'soc-icon-last')), array('href', ('https://www.yelp.com/biz/mag-lab-walnut'))), 'html5', 'UTF-8'); ?>></a>
+          </li>
+        </ul>
+      </div>
     </div>
     hello
 
