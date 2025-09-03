cd2e2c4aa069287fc12fb574a86cd1656f12023f
Kevin Yen <ky.17364@gmail.com>
2025-09-02 23:12:35 -0700

    fix: suppress deprecation warning for newer version of PHP

--- home/protected/maglab/app/main.php
+++ home/protected/maglab/app/main.php
@@ -1,5 +1,9 @@
 <?php
 
+// Suppress deprecation warnings to prevent output buffer issues
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', 0);
+
 require __DIR__ . '/../config/config.php';
 require __DIR__ . "/../vendor/autoload.php";
 #require __DIR__ . "/../lib/mthaml/Runtime/AttributeList.php";
